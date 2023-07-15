f6cbb77575076daaf78cc638a956f0e0b2d82fe8
Osman Alhassan <oalhassan847@gmail.com>
2023-07-14 18:17:42 -0700

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -2,7 +2,11 @@ class Solver
   def factorial(n)
     (1..n).reduce(:*) || 1
   end
+  def reverse(str)
+    str.reverse
+  end
 end
 
 newa = Solver.new();
 puts newa.factorial(5);
+puts newa.reverse('hello');
